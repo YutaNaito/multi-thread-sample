ee22e35c916b05f3e02b730e996fdb40e3aa8394
Yuta Naito <44762745+YutaNaito@users.noreply.github.com>
2024-11-20 15:00:13 +0900

    modify

--- rust/Deadlock/src/main.rs
+++ rust/Deadlock/src/main.rs
@@ -8,27 +8,39 @@ fn main() {
     let object1 = Arc::new(Mutex::new(0));
     let object2 = Arc::new(Mutex::new(0));
 
-    let object1_th1 = Arc::clone(&object1);
-    let object2_th1 = Arc::clone(&object2);
-    let object1_th2 = Arc::clone(&object2);
-    let object2_th2 = Arc::clone(&object1);
+    // let object1_th1 = Arc::clone(&object1);
+    // let object2_th1 = Arc::clone(&object2);
+    // let object1_th2 = Arc::clone(&object2);
+    // let object2_th2 = Arc::clone(&object1);
 
-    let handle1 = thread::spawn(move || {
-        println!("start th1");
-        let obj1 = object1_th1.lock().unwrap();
-        thread::sleep(Duration::from_millis(5));
-        let obj2 = object2_th1.lock().unwrap();
-        thread::sleep(Duration::from_millis(5));
-        println!("obj1:{}, obj2: {}", *obj1, obj2);
+    let handle1 = thread::spawn({
+        let object1 = object1.clone();
+        let object2 = object2.clone();
+        move || {
+            println!("start th1");
+            let obj1 = object1.lock().unwrap();
+            // let obj1 = object1_th1.lock().unwrap();
+            thread::sleep(Duration::from_millis(5));
+            let obj2 = object2.lock().unwrap();
+            // let obj2 = object2_th1.lock().unwrap();
+            thread::sleep(Duration::from_millis(5));
+            println!("obj1:{}, obj2: {}", *obj1, *obj2);
+        }
     });
 
-    let handle2 = thread::spawn(move || {
-        println!("start th2");
-        let obj1 = object1_th2.lock().unwrap();
-        thread::sleep(Duration::from_millis(5));
-        let obj2 = object2_th2.lock().unwrap();
-        thread::sleep(Duration::from_millis(5));
-        println!("obj1:{}, obj2: {}", *obj1, obj2);
+    let handle2 = thread::spawn({
+        let object1 = object1.clone();
+        let object2 = object2.clone();
+        move || {
+            println!("start th2");
+            let obj2 = object2.lock().unwrap();
+            // let obj2 = object2_th2.lock().unwrap();
+            thread::sleep(Duration::from_millis(5));
+            let obj1 = object1.lock().unwrap();
+            // let obj1 = object1_th2.lock().unwrap();
+            thread::sleep(Duration::from_millis(5));
+            println!("obj1:{}, obj2: {}", *obj1, *obj2);
+        }
     });
 
     handle1.join().unwrap();
